c8da23ae24519b0439310cde47b89bab69028f31
Maksym <maksskazka1997@gmail.com>
2026-05-14 20:40:35 +0200

    fix

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { api } from '../api';
 import { cookies } from 'next/headers';
-import { isAxiosError } from 'axios';
+import axios from 'axios';
 import { logErrorResponse } from '../_utils/utils';
 
 export async function GET(request: NextRequest) {
@@ -17,15 +17,15 @@ export async function GET(request: NextRequest) {
 
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
-    if (isAxiosError(error)) {
+    if (axios.isAxiosError(error)) {
       logErrorResponse(error.response?.data);
       return NextResponse.json(
-        { error: error.message, response: error.response?.data },
-        { status: error.status }
+        { message: error.message, data: error.response?.data },
+        { status: error.response?.status || 500 }
       );
     }
     logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
 
@@ -43,14 +43,14 @@ export async function PATCH(request: NextRequest) {
 
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
-    if (isAxiosError(error)) {
+    if (axios.isAxiosError(error)) {
       logErrorResponse(error.response?.data);
       return NextResponse.json(
-        { error: error.message, response: error.response?.data },
-        { status: error.status }
+        { message: error.message, data: error.response?.data },
+        { status: error.response?.status || 500 }
       );
     }
     logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
