6625f1acf2455c3ba419603dcdbb55205044f4ef
Maksym <maksskazka1997@gmail.com>
2026-05-17 17:37:27 +0200

    fix

--- app/api/users/me/route.ts
+++ app/api/users/me/route.ts
@@ -1,10 +1,10 @@
-export const dynamic = 'force-dynamic';
-
 import { NextResponse } from 'next/server';
-import { api } from '../../api';
 import { cookies } from 'next/headers';
-import { logErrorResponse } from '../../_utils/utils';
+import { api } from '@/lib/api/api'; 
+import { logErrorResponse } from '@/lib/api/logErrorResponse';
 import { isAxiosError } from 'axios';
+
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
@@ -15,40 +15,74 @@ export async function GET() {
         Cookie: cookieStore.toString(),
       },
     });
-    return NextResponse.json(res.data, { status: res.status });
+
+    return NextResponse.json(res.data, {
+      status: res.status,
+    });
   } catch (error) {
+    logErrorResponse(error, 'API_USERS_ME_GET');
+
     if (isAxiosError(error)) {
-      logErrorResponse(error.response?.data);
       return NextResponse.json(
-        { error: error.message, response: error.response?.data },
-        { status: error.status }
+        {
+          error: error.message,
+          response: error.response?.data,
+        },
+        {
+          status: error.response?.status || 500,
+        }
       );
     }
-    logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+
+    return NextResponse.json(
+      {
+        error: 'Something went wrong',
+        response: null,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
 
-export async function PATCH(request: Request) {
+export async function PATCH(req: Request) {
   try {
+    const body = await req.json();
     const cookieStore = await cookies();
-    const body = await request.json();
 
     const res = await api.patch('/users/me', body, {
       headers: {
         Cookie: cookieStore.toString(),
       },
     });
-    return NextResponse.json(res.data, { status: res.status });
+
+    return NextResponse.json(res.data, {
+      status: res.status,
+    });
   } catch (error) {
+    logErrorResponse(error, 'API_USERS_ME_PATCH');
+
     if (isAxiosError(error)) {
-      logErrorResponse(error.response?.data);
       return NextResponse.json(
-        { error: error.message, response: error.response?.data },
-        { status: error.status }
+        {
+          error: error.message,
+          response: error.response?.data,
+        },
+        {
+          status: error.response?.status || 500,
+        }
       );
     }
-    logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+
+    return NextResponse.json(
+      {
+        error: 'Something went wrong',
+        response: null,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
