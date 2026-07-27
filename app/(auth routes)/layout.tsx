80367202d9a80e61846592c8c644d646d8005e26
Maksym <maksskazka1997@gmail.com>
2026-07-27 08:15:55 +0200

    last-fix

---
+++ app/(auth routes)/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
+  return (
+    <section>
+      {children}
+    </section>
+  );
+}
